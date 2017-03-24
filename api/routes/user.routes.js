3d636ebafebb0f9bf634b08c53aaf020b8a5468c
Joseph Lao <josephlao.1398@gmail.com>
2017-03-24 19:23:36 +0800

    Base user setup

---
+++ api/routes/user.routes.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function (app) {
+    var usersController = require('../controllers/users.controller');
+
+
+    // usersController Routes
+    app.route('/users')
+        .get(usersController.list_all_users)
+        .post(usersController.create_a_user);
+
+
+    app.route('/users/:userId')
+        .get(usersController.fetch_a_user)
+        .put(usersController.update_a_user)
+        .delete(usersController.delete_a_user);
+}
