f6fdcc7d62815187fad64e67704a30bbdbaf0ac5
Joseph Lao <josephlao.1398@gmail.com>
2017-04-04 14:59:00 +0800

    [Feature-01] Additional update

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,54 +1,50 @@
-'use strict';
+(function () {
+    'use strict';
 
+    var mongoose = require('mongoose'),
+        Users = mongoose.model('Users');
 
-var mongoose = require('mongoose'),
-    Users = mongoose.model('Users');
+    exports.list_all_users = function (req, res) {
+        Users.find({}, function (err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    };
 
-exports.list_all_users = function (req, res) {
-    Users.find({}, function (err, user) {
-        if (err)
-            res.send(err);
-        res.json(user);
-    });
-};
+    exports.create_a_user = function (req, res) {
+        var new_user = new Users(req.body);
+        new_user.save(function (err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    };
 
+    exports.fetch_a_user = function (req, res) {
+        Users.findById(req.params.userId, function (err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    };
 
+    exports.update_a_user = function (req, res) {
+        Users.findOneAndUpdate(req.params.userId, req.body, { new: true }, function (err, user) {
+            if (err)
+                res.send(err);
+            res.json(user);
+        });
+    };
 
+    exports.delete_a_user = function (req, res) {
+        Users.remove({
+            _id: req.params.userId
+        }, function (err, user) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'User successfully deleted' });
+        });
+    };
+})();
 
-exports.create_a_user = function (req, res) {
-    var new_user = new Users(req.body);
-    new_user.save(function (err, user) {
-        if (err)
-            res.send(err);
-        res.json(user);
-    });
-};
-
-
-exports.fetch_a_user = function (req, res) {
-    Users.findById(req.params.userId, function (err, user) {
-        if (err)
-            res.send(err);
-        res.json(user);
-    });
-};
-
-
-exports.update_a_user = function (req, res) {
-    Users.findOneAndUpdate(req.params.userId, req.body, { new: true }, function (err, user) {
-        if (err)
-            res.send(err);
-        res.json(user);
-    });
-};
-
-
-exports.delete_a_user = function (req, res) {
-    Users.remove({
-        _id: req.params.userId
-    }, function (err, user) {
-        if (err)
-            res.send(err);
-        res.json({ message: 'User successfully deleted' });
-    });
-};
--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    mongoose = require('mongoose'),
-    Task = require('./api/models/users.model'),
-    bodyParser = require('body-parser');
+(function () {
+    'use strict';
 
-mongoose.Promise = global.Promise;
-var connection = 'mongodb://username:password@ds139470.mlab.com:39470/api-dtr';
-mongoose.connect(connection, function (err) {
-    if (err) throw err;
-    console.log('Successfully connected to Database')
-});
+    var express = require('express'),
+        app = express(),
+        port = process.env.PORT || 3000,
+        mongoose = require('mongoose'),
+        Task = require('./api/models/users.model'),
+        bodyParser = require('body-parser');
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-var routes = require('./api/routes/user.routes');
-routes(app);
+    mongoose.Promise = global.Promise;
+    var connection = 'mongodb://username:password@ds139470.mlab.com:39470/api-dtr';
+    mongoose.connect(connection, function (err) {
+        if (err) throw err;
+        console.log('Successfully connected to Database');
+    });
 
 
-app.listen(port);
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
-app.use(function (req, res) {
-    res.status(404).send({ url: req.originalUrl + ' not found' });
-});
-console.log('Server started on: ' + port);
+    var routes = require('./api/routes/user.routes');
+    routes(app);
+
+
+    app.listen(port);
+
+    app.use(function (req, res) {
+        res.status(404).send({ url: req.originalUrl + ' not found' });
+    });
+    console.log('Server started on: ' + port);
+
+})();
 
